e5425ace1d152ab3dda48016e57914709990f2ad
neopragma <davenicolette@gmail.com>
2022-12-12 16:16:55 -0700

    Moving content to multiple files

--- build-kwic-index.rb
+++ build-kwic-index.rb
@@ -9,9 +9,9 @@ File.foreach("docs/contents.md") { |line|
   matches.captures.each() { |capture|
     words = capture.split(' ')
     link = line.match(extract_link)
-    kwic_entry = ['..............................']
+    kwic_entry = ['. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ']
     words.each_with_index() { |word,ix|
-        kwic_entry << words[0..ix-1].join(' ').rjust(30,'.') unless ix == 0
+        kwic_entry << words[0..ix-1].join(' ').rjust(60,'.') unless ix == 0
         kwic_entry << "|[#{word}]#{link}"
         kwic_entry += words[ix+1..words.length-1] unless ix == words.length
         puts "|#{kwic_entry.join(' ')}|\n"
