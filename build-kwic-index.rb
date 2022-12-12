182a9c35527588aebfbe10b774ac6e728b608266
neopragma <davenicolette@gmail.com>
2022-12-12 16:18:42 -0700

    Moving content to multiple files

--- build-kwic-index.rb
+++ build-kwic-index.rb
@@ -2,19 +2,17 @@
 extract_text_between_square_brackets = /(?<=\[)(.*?)(?=\])/
 extract_link = /\((.*?)\)/
 
-puts "|one|two|\n|---|---|\n"
-
 File.foreach("docs/contents.md") { |line| 
   matches = line.match(extract_text_between_square_brackets)
   matches.captures.each() { |capture|
     words = capture.split(' ')
     link = line.match(extract_link)
-    kwic_entry = ['. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ']
+    kwic_entry = []
     words.each_with_index() { |word,ix|
         kwic_entry << words[0..ix-1].join(' ').rjust(60,'.') unless ix == 0
-        kwic_entry << "|[#{word}]#{link}"
+        kwic_entry << "[#{word}]#{link}"
         kwic_entry += words[ix+1..words.length-1] unless ix == words.length
-        puts "|#{kwic_entry.join(' ')}|\n"
+        puts "#{kwic_entry.join(' ')}\n"
         kwic_entry.clear
     }
   } unless matches == nil
