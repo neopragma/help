20cff09f86fd7ebe965814699242bf63e4bd55a8
neopragma <davenicolette@gmail.com>
2022-12-14 08:09:01 -0700

    Testing KWIC index formatting

--- build-kwic-index.rb
+++ build-kwic-index.rb
@@ -1,21 +1,37 @@
 # Capture text between square brackets 
 extract_text_between_square_brackets = /(?<=\[)(.*?)(?=\])/
-extract_link = /\((.*?)\)/
+extract_link_text = /\((.*?)\)/
+left_column = []
+right_column =  []
+page_heading = '# Keyword In Context Index'
+table_start = '<table><thead><tr><th colspan="2">Keywords In Context Index</th></thead><tbody><tr><td>'
+table_cell = '</td><td>'
+table_row = '</td></tr><tr><td>'
+table_end = '</tr></tbody></table>'
 
-#puts '``'
+puts page_heading
+puts table_start
 File.foreach("docs/contents.md") { |line| 
   matches = line.match(extract_text_between_square_brackets)
-  matches.captures.each() { |capture|
+  matches.captures.each() { |capture| 
     words = capture.split(' ')
-    link = line.match(extract_link)
-    kwic_entry = []
+    link_text = line.match(extract_link_text)[1]
+    kwic_entry_left = []
+    kwic_entry_right = []
     words.each_with_index() { |word,ix|
-        kwic_entry << words[0..ix-1].join(' ').rjust(60,'.') unless ix == 0
-        kwic_entry << "[#{word}]#{link}"
-        kwic_entry += words[ix+1..words.length-1] unless ix == words.length
-        puts "#{kwic_entry.join(' ')}  \n"
-        kwic_entry.clear
+        kwic_entry_left << words[0..ix-1].join(' ') unless ix == 0
+        kwic_entry_right << "<a href=\"#{link_text}\">#{word}</a> "
+        kwic_entry_right << words[ix+1..words.length-1] unless ix == words.length
+        left_column << "#{kwic_entry_left.join(' ')}"
+        right_column << "#{kwic_entry_right.join(' ')}"
+        kwic_entry_left = []
+        kwic_entry_right = []
     }
   } unless matches == nil
-#  puts '``'
 }
+
+left_column.each_with_index() { |left_entry, ix|
+puts "#{table_row}#{left_entry}#{table_cell}#{right_column[ix]}"
+}
+
+puts table_end 
